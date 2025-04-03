db02bf29df2d4d73312047cab92242877188e9df
Aritree Hoque <aritree.hoque@gmail.com>
2025-04-02 23:17:18 -0400

    Adding comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Greeting from './components/Greeting';
+import Greeting from './components/Greeting'; // Task2- Importing and using the Greeting component.
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,9 +1,9 @@
-function Greeting() {
+function Greeting() { 
     return (
     <div>
         <h2>Hello from the Greeting Component!</h2>
-        <p>This is your first custom component in React.</p>
+        <p>This is your first custom component in React.</p> 
     </div>
-    );
+    ); // Functional React component returning comment
 }
-export default Greeting;
+export default Greeting; // Task1
